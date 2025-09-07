783f10c9d98d0e83d338ad941b3aff0b55c3572b
dilminawodya2001 <dilminawodya2001@gmail.com>
2025-09-07 19:13:49 +0530

    third

--- StudentManagementJDBC/src/DBConnection.java
+++ StudentManagementJDBC/src/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/studentdb";
     private static final String USER = "root"; // your DB username
-    private static final String PASSWORD = "200165200050NAWYA@31"; // your DB password
+    private static final String PASSWORD = "1234"; // your DB password
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
